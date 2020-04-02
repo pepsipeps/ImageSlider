1095c4fa40dfdd6bf69d81eca8bc8be1a58567df
pepsipeps <63060786+pepsipeps@users.noreply.github.com>
2020-04-03 00:47:27 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    ground  = require("./models/ground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    groundRoutes = require("./routes/grounds"),
+    indexRoutes      = require("./routes/index")
+ 
+var url = process.env.DATABASEURL || "mongodb://localhost/Image_slider";
+mongoose.connect(url);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Hello!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
+
+app.use("/", indexRoutes);
+app.use("/grounds", groundRoutes);
+app.use("/grounds/:id/comments", commentRoutes);
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Woohoo!");
+});
