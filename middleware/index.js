11d314706ecbd4524e1aa47444b1a7fcb0957a95
pepsipeps <63060786+pepsipeps@users.noreply.github.com>
2020-04-03 01:01:38 +0200

    Add files via upload

---
+++ middleware/index.js
@@ -0,0 +1,58 @@
+var ground = require("../models/ground");
+var Comment = require("../models/comment");
+
+
+var middlewareObj = {};
+
+middlewareObj.checkgroundOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        ground.findById(req.params.id, function(err, foundground){
+           if(err){
+               req.flash("error", "ground not found");
+               res.redirect("back");
+           }  else {
+              
+            if(foundground.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+               res.redirect("back");
+           }  else {
+              
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
